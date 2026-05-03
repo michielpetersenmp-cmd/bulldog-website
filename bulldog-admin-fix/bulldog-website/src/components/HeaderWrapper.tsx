7be10fedca814a02e3d5ab73725f80bce566c0e2
Michiel <michielpetersen.mp@gmail.com>
2026-05-03 21:31:46 +0200

    Verwijder foute submappen

--- bulldog-admin-fix/bulldog-website/src/components/HeaderWrapper.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import { usePathname } from "next/navigation";
-import Header from "./Header";
-
-export default function HeaderWrapper() {
-  const pathname = usePathname();
-  if (pathname.startsWith("/admin")) return null;
-  return <Header />;
-}
